fdacec0bb9ede8db677ff210539d729fad8faf9f
saya <sa072688@gmail.com>
2018-01-26 22:00:49 +0900

    position IValue, improve SelfDesc attribute

--- Assets/Scripts/Editor/CustomDrawer/SelfDescAttributeDrawer.cs
+++ Assets/Scripts/Editor/CustomDrawer/SelfDescAttributeDrawer.cs
@@ -13,10 +13,19 @@ public sealed class SelfDescAttributeDrawer : OdinAttributeDrawer<SelfDescAttrib
         // 檢查是否為 ISelfDescriptable
         if (typeof(ISelfDescriptable).IsAssignableFrom(property.ValueEntry.TypeOfValue))
         {
-            ISelfDescriptable value = property.ValueEntry.WeakSmartValue as ISelfDescriptable;
-            string msg = value.Desc();
             this.CallNextDrawer(property, label);
-            EditorGUILayout.LabelField(msg);
+            // Draw ISelfDescriptable
+            ISelfDescriptable value = property.ValueEntry.WeakSmartValue as ISelfDescriptable;
+            string msg = string.Format("{1}: {0}", value.Desc(), property.Name);
+            Color bgcTemp = GUI.color;
+            GUI.color = Color.magenta;
+            {
+                Rect r = EditorGUILayout.GetControlRect();
+                EditorGUI.DrawRect(r, Color.magenta);
+                EditorGUI.LabelField(r, msg);
+                //EditorGUILayout.LabelField(msg);
+            }
+            GUI.color = bgcTemp;
         }
         // 如果不是，印出錯誤
         else
@@ -24,31 +33,5 @@ public sealed class SelfDescAttributeDrawer : OdinAttributeDrawer<SelfDescAttrib
             SirenixEditorGUI.ErrorMessageBox("此欄型別並非 ISelfDescriptable！");
             this.CallNextDrawer(property, label);
         }
-
-        /* 封印
-        // Get and create string member helper context.
-        PropertyContext<StringMemberHelper> context;
-        if (property.Context.Get(this, "StringHelper", out context))
-        {
-            context.Value = new StringMemberHelper(property.ParentType, attribute.Name);
-        }
-
-        // Display error
-        if (context.Value.ErrorMessage != null)
-        {
-            SirenixEditorGUI.ErrorMessageBox(context.Value.ErrorMessage);
-            this.CallNextDrawer(property, label);
-        }
-        else
-        {
-            EditorGUILayout.BeginHorizontal();
-            this.CallNextDrawer(property, null);
-
-            // Get the string from the string member helper.
-            EditorGUILayout.PrefixLabel(context.Value.GetString(property));
-
-            EditorGUILayout.EndHorizontal();
-        }
-        */
     }
 }
---
+++ Assets/Scripts/Shot/PositionReference/MulPositionReference.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulPositionReference : PositionReference
+{
+    public PositionReference Left = new CustomVectorPositionReference();
+    public IValue<ShotMiko> Right = new ConstValue<ShotMiko>(1f);
+
+    public override Vector3 GetPos(ShotMiko mikosama)
+    {
+        return Left.GetPos(mikosama) * Right.Eva(mikosama);
+    }
+}
